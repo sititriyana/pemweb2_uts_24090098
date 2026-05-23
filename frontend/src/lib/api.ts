a871d3966a94a6252f291a286fbc2f89baa40c5c
ann <sttryanaa>
2026-05-23 21:42:15 +0700

    Fix 404 routing di Vercel

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://pemweb2uts24090098-production.up.railway.app/api',
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
