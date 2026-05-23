fa3578317f1992eb7e50dbe3fecb0d7ba6b515fe
ann <sttryanaa>
2026-05-23 20:27:14 +0700

    uts_pemweb2_sttriyana_24090098

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://uts-pemweb2-24090098-lumt.vercel.app/api',
-})
+  baseURL: 'http://localhost:5000/api',
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
